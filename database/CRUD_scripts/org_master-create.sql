a62f08b6c22871884f52ceb5aaea473d2b440e0d
sean <sean_github@pm.me>
2024-10-28 15:53:09 -0700

    Updates to crud table fields to match dbml drafts

--- database/CRUD_scripts/org_master-create.sql
+++ database/CRUD_scripts/org_master-create.sql
@@ -1,8 +1,10 @@
 CREATE TABLE org_master (
-    org_id SERIAL PRIMARY KEY,
+    org_uuid UUID PRIMARY KEY,
     org_name VARCHAR(100) NOT NULL,
-    org_type_id INTEGER REFERENCES org_type_desc(org_type_id),
-    org_status_id INTEGER REFERENCES org_status_desc(org_status_id),
+    org_name_full VARCHAR(150) NOT NULL,
+    org_type_id INTEGER REFERENCES org_type(org_type_id),
+    org_status_id INTEGER REFERENCES org_status(org_status_id),
+    parent_org_uuid UUID REFERENCES org_master(org_uuid),
     updated_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(50) DEFAULT CURRENT_USER,
     created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
