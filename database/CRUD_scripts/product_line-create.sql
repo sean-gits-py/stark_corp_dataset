a3973a97c1a01f6ba4bf6d279d9d3b3b3ddad2d2
Sean Ainsworth <37087075+sean-gits-py@users.noreply.github.com>
2024-10-29 23:35:49 -0700

    Adds and edits to crud scripts

--- database/CRUD_scripts/product_line-create.sql
+++ database/CRUD_scripts/product_line-create.sql
@@ -4,8 +4,11 @@ CREATE TABLE IF NOT EXISTS product_line (
     product_id INTEGER NOT NULL,
     sku_id INTEGER NOT NULL,
     line_name VARCHAR(255),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT true,
+    created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_ts TIMESTAMP WITH TIME ZONE,
+    updated_by VARCHAR(100),
     CONSTRAINT fk_product
         FOREIGN KEY (product_id)
         REFERENCES product(product_id)
--- database/CRUD_scripts/product_sub_category-create.sql
+++ database/CRUD_scripts/product_sub_category-create.sql
@@ -3,10 +3,13 @@ CREATE TABLE IF NOT EXISTS subcategories (
     subcategory_id SERIAL PRIMARY KEY,
     category_id INTEGER NOT NULL,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT true,
+    created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(100),
+    updated_ts TIMESTAMP WITH TIME ZONE,
+    updated_by VARCHAR(100),
     CONSTRAINT fk_category
         FOREIGN KEY (category_id)
-        REFERENCES categories(category_id)
+        REFERENCES category(category_id)
         ON DELETE CASCADE
 );
